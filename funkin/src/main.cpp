27ba5c1e9f382478edcc846f9e5bddac0cff3555
Dusty <100844923+DustyDevz@users.noreply.github.com>
2026-06-30 00:59:27 -0400

    qml

--- funkin/src/main.cpp
+++ funkin/src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+
+    QString qmlPath = QCoreApplication::applicationDirPath() + "/test.qml";
+    engine.load(QUrl::fromLocalFile(qmlPath));
+
     std::cout << "Test" << std::endl;
-    return 0;
+    return app.exec();
 }
