b8e369f90a88562b738f9d6ca8e91e12df0709f6
Dusty <100844923+DustyDevz@users.noreply.github.com>
2026-07-18 21:44:10 -0400

    bla bla

---
+++ funkin/include/render/graphics/gfx_device.hpp
@@ -0,0 +1,13 @@
+/*
+ FunkinC++ Engine
+ Copyright (©) 2026 Dusty. All rights reserved.
+
+ This program is licensed under the GNU Affero General Public License v3.0 and is distributed WITHOUT ANY WARRANTY.
+ For a copy, see <https://www.gnu.org/licenses/agpl-3.0.html>.
+*/
+
+#pragma once
+#include <RefCntAutoPtr.hpp>
+#include <RenderDevice.h>
+#include <DeviceContext.h>
+#include <EngineFactoryVk.h>
